9c02fb2ebf3edf604fd8f855586ada032a1ad579
Seth <sethwilfinger@gmail.com>
2024-11-21 19:41:06 +0000

    Added Collision Detection (Fail Condition/Drafted Collision with trophy)

--- snake.c
+++ snake.c
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
-// to compile:
-//      gcc snake.c -o snake -l curses
 /*
+to compile: gcc snake.c -o snake -l curses
+
     references:
         https://cs.ccsu.edu/~stan/classes/CS355/notes/09-EventDrivenProgramming.html
         https://cs.ccsu.edu/~stan/classes/CS355/notes/08-TerminalControl.html
@@ -40,36 +40,14 @@
     signals:
         sync: inside process (division by 0)
         async: outside proccess (Ctrl-C)
-    
-
-    Project Details (from Stan's requirements)
-    https://cs.ccsu.edu/~stan/classes/CS355/Project-FA24.html
-    The snake pit:
-        The snake pit is the area where the snake can move.
-        The snake pit must utilize all available space of the current terminal window.
-        There must be a visible border delineating the snake pit.
-    The initial length of the snake is three characters.
-        Initial direction of the snake's movement is chosen randomly.
-        The user can press one of the four arrow keys to change the direction of the snake's movement.
-        The snake's speed is proportional to its length.
-    The trophies:
-        Trophies are represented by a digit randomly chosen from 1 to 9.
-        There's always exactly one trophy in the snake pit at any given moment.
-        When the snake eats the trophy, its length is increased by the corresponding number of characters.
-        A trophy expires after a random interval from 1 to 9 seconds.
-        A new trophy is shown at a random location on the screen after the previous one has either expired or is eaten by the snake.
-    The gameplay:
-        The snake dies and the game ends if:
-        It runs into the border; or
-        It runs into itself; or
-        The user attempts to reverse the snake's direction.
-        The user wins the game if the snake's length grows to the length equal to half the perimeter of the border.
 
 */
 // functions initialization
 void createBorders();
 void createSnake();
 void input();
+void movesnake();
+void collision();
 int getRand(int max,int min);
 
 struct vector2{
@@ -77,37 +55,102 @@ struct vector2{
     int y;
 };
 
-// Snake info; (i wonder if we allow diagonal movement?)
+// Snake info; (i wonder if we should allow diagonal movement?) (probably not)
 int snakeLen = 5; // initial length
 struct vector2 moveDirection; // this will be a vector 2;
 struct vector2 snake[128]; // contains info on the actual snake
 
 // Game info
+int isgameWon = 0; // (bool) exit if game won
+int isgameRun = 1; // (bool) exit if game ended
+int winCondition;
+char head = 'X'; // Snake's Head
+char body = '*'; // Snake's Body
 
 int main(){
-    int isgameWon = 0; // exit if game won
-    int isgameRun = 1; // exit if game ended
     srand(time(NULL)); // seeds rand number generator with current time atm
-    LINES = 20; // Define borders
-    COLS = 40; // Define border
-
     // Start Creating Game
-    initscr(); // Create Screen
+    initscr();
     createBorders();
     createSnake();
-    
-    sleep(3);
+    winCondition = 2*(LINES + COLS);
+    do {
+        // Here we should probably
+        //input();        // get input -> changes move Direction based off input: wasd/arrowkeys
+        collision();    // check collisions
+        movesnake();      // move snake -> move the snake head and the body follows the position it was last in
+        refresh();
+        usleep(1000000/4); // microseconds = 1sec => 1,000,000 micro
+        // (1/2) of a second ^
+    }while(isgameWon == 0 && isgameRun != 0);
+    char str[100];
+    if (isgameWon == 1){
+        strcpy(str, "YOU WON!");
+        // Show Player Won?
+    }else{
+        strcpy(str,"YOU LOST!");
+        // Show Player Lost
+    }
+    move(LINES/2,(COLS - strlen(str))/2);
+    addstr(str);
+    refresh();
+
+    sleep(5);
     endwin();
     return 0;
 }
+void collision(){
+    move(snake[0].x + moveDirection.x,snake[0].y + moveDirection.y);
+    chtype chT = winch(stdscr);
+    char ch = chT & A_CHARTEXT; // get the character of where the head in going
+    
+    if (ch == head || ch == body || ch == '#'){
+        isgameRun = 0;
+    }else if(ch != ' ' ){
+        // Assume the snake ran into a trophy
+        //int trophyVal = ch - '0'; // convert char num to value by sub ascii value of 0
+        //snakeLen += trophyVal;
+        // generate new trophy on claim
+        printf("%c\n",ch);
+        // check winCondition on interaction with char
+        if (snakeLen > winCondition){
+            isgameWon = 1;
+        }
+    }
+    
+}
+void movesnake(){
+    struct vector2 prev;
+    for (int i = 0; i < snakeLen; i++){
+        int tempx = snake[i].x;
+        int tempy = snake[i].y;
+        char bodypart = body;
+        if (i == 0){ //Snake Head
+            bodypart = head;
+            prev.x = tempx + moveDirection.x;
+            prev.y = tempy  + moveDirection.y;
+        }
 
-int getRand(int max,int min){ // returns random number in between the max and min
+        snake[i].x = prev.x;
+        snake[i].y = prev.y;
+        prev.x = tempx;
+        prev.y = tempy;
+        move(snake[i].x, snake[i].y);
+        addch(bodypart);
+        move(prev.x,prev.y);
+        addch(' ');
+    }
+}
+
+int getRand(int max,int min){ // Seth
+    // returns random number in between the max and min
     return (rand() % (max - min + 1)) + min;
 }
 
-void createBorders(){ // LINES COLS globals
-    // x borders: x == 0 || x == LINES -1
-    // y borders: y == 0 || y == COLS  -1
+void createBorders(){ // Seth
+    // LINES COLS globals
+    // x borders: x == 0 || x == LINES-1
+    // y borders: y == 0 || y == COLS-1
     for (int x = 0; x<LINES; x++){ // No need to nested loop cause then you are looping over unecessary positions on the screen
         for (int y = 0; y<COLS; y++){
             if ((x == 0 || x == LINES-1) || (y == 0 || y == COLS -1)){ // Same comment as above but this is just columns
@@ -118,17 +161,16 @@ void createBorders(){ // LINES COLS globals
     }
 }
 
-void createSnake(){
-    // Get Starting moveDirection
+void createSnake(){ // Seth
+    // Get Random Starting moveDirection
     int r = getRand(4, 1);
-    printf("rand:%d\n",r);
     switch (r) { // random direction
         case 1:
-            moveDirection.x = 1;
+            moveDirection.x = -1;
             moveDirection.y = 0;
             break;
         case 2:
-            moveDirection.x = -1;
+            moveDirection.x = 1;
             moveDirection.y = 0;
             break;
         case 3:
@@ -140,19 +182,20 @@ void createSnake(){
             moveDirection.y = 1;
             break;
     }
-    printf("MoveDirection x:%d,y%d\n",moveDirection.x,moveDirection.y);
+
     // Move to Center and Add Head
     int curX = LINES/2;
     int curY = COLS/2;
     move(curX,curY);
-    addch('0');
+    addch(head);
     snake[0].x = curX;
     snake[0].y = curY; // Sets the Head to center
+
     for (int i = 1; i < snakeLen; i++){ // Build the rest of the body in the opposite of the move direction
         snake[i].x = snake[i-1].x - moveDirection.x;
         snake[i].y = snake[i-1].y - moveDirection.y;
         move(snake[i].x, snake[i].y);
-        addch('o');
+        addch(body);
     }
 
     refresh();
